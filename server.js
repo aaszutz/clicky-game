bd42fcff471ea23e60c0c165ef8f0d7037a47032
Alyssa Szutz <aaszutz@gmail.com>
2019-06-05 20:27:11 -0400

    deployment

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3001;
 const app = express();
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(src));
-app.use(express.static(path.join(src, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("clicky-game/build"));
+}
 // app.get('/*', function (req, res) {
 //   res.sendFile(path.join(__public, 'build', 'index.html'));
 // });
